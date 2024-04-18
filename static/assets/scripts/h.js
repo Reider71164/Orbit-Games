a2d8e42f0722cac55d223c6efe99aa6123f712e2
Reider71164 <91398971+Reider71164@users.noreply.github.com>
2024-04-17 22:17:39 -0700

    Update h.js

--- static/assets/scripts/h.js
+++ static/assets/scripts/h.js
@@ -164,16 +164,12 @@ document.addEventListener('DOMContentLoaded', function (event) {
 })
 
 let splashtext = [
-  'Over 8 Million Users since 2023',
-  'Fastest growing proxy server',
-  'Made by xBubbo',
-  'Check out discord.gg/interstellar :)',
+  'Brought to you by Raider',
+  'Good unb games!',
+  'Made by Raider',
+  'Check out https://discord.gg/v54gnj4MGn :)',
   'Thanks for using the site',
-  'Follow us on Tiktok (@useinterstellar)',
-  'Subscribe to us on YouTube (@unblocking)',
-  'Subscribe to my Youtube (@xbubbo)',
   'Check out the settings page',
-  'Check out our Patreon (https://www.patreon.com/gointerstellar)',
 ]
 
 document.getElementById('splash').innerText = splashtext[Math.floor(Math.random() * splashtext.length)]
